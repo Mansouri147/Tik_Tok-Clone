1f75a2d48ade89ab7b53ffea8a6193a021fe86a7
Ala Mansouri <mansouriwhafidoumansouri@gmail.com>
2020-08-29 21:55:11 +0100

    tik tok clone finished wooooow

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ_uPVYa9Jq9QX5YseYk0Hkb72nWiR5Pc",
-  authDomain: "tik-tok-clo.firebaseapp.com",
-  databaseURL: "https://tik-tok-clo.firebaseio.com",
-  projectId: "tik-tok-clo",
-  storageBucket: "tik-tok-clo.appspot.com",
-  messagingSenderId: "1015629263679",
-  appId: "1:1015629263679:web:e73332903f08a2ea30598a",
-  measurementId: "G-1THHHDDY43",
+  apiKey: "AIzaSyB871pYR99PHjP4_iJrgyY74E-GYq1Gu6M",
+  authDomain: "tik-tok-clon-e.firebaseapp.com",
+  databaseURL: "https://tik-tok-clon-e.firebaseio.com",
+  projectId: "tik-tok-clon-e",
+  storageBucket: "tik-tok-clon-e.appspot.com",
+  messagingSenderId: "235677857823",
+  appId: "1:235677857823:web:b4b261840454699940fd2a",
+  measurementId: "G-Q172PBZ7ZX"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
